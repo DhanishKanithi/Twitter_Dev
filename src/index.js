eb9ee04db3e043e4efaaa26a382e537ed2ca1806
Dhanish Kanithi <dhanishkanithi@gmail.com>
2024-12-29 14:27:09 +0530

    Added toggle like api

--- src/index.js
+++ src/index.js
@@ -107,6 +107,8 @@ app.listen(3000, async () => {
     // });
     // console.log(tweet);
 
+    
+    /*
     const userRepo = new UserRepository();
     const tweetRepo = new TweetRepository()
     const tweets = await tweetRepo.getAll(0,10);
@@ -119,5 +121,6 @@ app.listen(3000, async () => {
 
     const likeService = new LikeService();
     await likeService.toggleLike(tweets[0].id, 'Tweet', users[0].id);
+    */
 
 });
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const {createTweet} = require('../../controllers/tweet-controller');
-
+const {toggleLike} = require('../../controllers/like-controller');
 const router = express.Router();
 
 router.post('/tweets', createTweet);
+
+router.post('/likes/toggle', toggleLike ); 
 
 module.exports = router;
